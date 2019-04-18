4a6a78bdc5d8887daa926065572d61d9497fc8b6
Cho-Ching Lu <chochinlu@gmail.com>
2019-04-18 17:52:30 +0800

    Add basic main layout

--- src/App.js
+++ src/App.js
@@ -4,27 +4,43 @@ import Menu from './components/Menu';
 import Information from './components/Information';
 import Flight from './components/Flight';
 import AddFlightForm from './components/AddFlightForm';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import blue from '@material-ui/core/colors/blue';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: blue
+const styles = theme => ({
+  row: {
+    padding: theme.spacing.unit
+  },
+  paper: {
+    margin: theme.spacing.unit,
+    padding: theme.spacing.unit * 2
   }
 });
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <MuiThemeProvider theme={theme}>
+      <div>
         <CssBaseline />
         <Menu />
-        <Information />
-        <Flight />
-        <AddFlightForm />
-      </MuiThemeProvider>
+        <Grid container spacing={0} className={classes.row}>
+          <Grid item xs={12} sm={8}>
+            <Paper className={classes.paper}>
+              <Information />
+              <Flight />
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Paper className={classes.paper}>
+              <AddFlightForm />
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
