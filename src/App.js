af8a833b113b5ca39eda8eca169e514949b8a649
Cho-Ching Lu <chochinlu@gmail.com>
2019-04-18 15:17:23 +0800

    Add meterial ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Menu from './components/Menu';
 import Information from './components/Information';
 import Flight from './components/Flight';
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <CssBaseline />
         <Menu />
         <Information />
         <Flight />
