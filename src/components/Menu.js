c31771cf5d69bfa2c7620912a097b8e865a239ed
Cho-Ching Lu <chochinlu@gmail.com>
2019-04-20 19:21:26 +0800

    Add date info

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,22 +5,22 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import AddIcon from '@material-ui/icons/Add';
 import IconButton from '@material-ui/core/IconButton';
+import { format } from 'date-fns';
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1
-  },
   grow: {
     flexGrow: 1
   }
 });
 
-const Menu = ({ classes }) => (
-  <div className={classes.root}>
+const dayFormat = today => format(today, 'EEE, MMM dd');
+
+const Menu = ({ classes, today = new Date() }) => (
+  <div className={classes.grow}>
     <AppBar position="static" color="default">
       <Toolbar>
         <Typography variant="h6" color="inherit" className={classes.grow}>
-          Flight Table
+          Flight Table ({dayFormat(today)})
         </Typography>
         <IconButton aria-label="Add" color="primary">
           <AddIcon />
