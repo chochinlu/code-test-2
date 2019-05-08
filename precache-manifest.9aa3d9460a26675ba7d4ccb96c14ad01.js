d03c07dfb59361cc1addbf60413c2467d435023f
Cho-Ching Lu <chochinlu@gmail.com>
2019-05-08 18:40:53 +0800

    Updates

---
+++ precache-manifest.9aa3d9460a26675ba7d4ccb96c14ad01.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4d788d3f8364c2047ef1c2efc7cc08",
+    "url": "/code-test-redux/index.html"
+  },
+  {
+    "revision": "7c4d2ce2f4a45bbe69d2",
+    "url": "/code-test-redux/static/js/2.bd82ed9c.chunk.js"
+  },
+  {
+    "revision": "4f42e1fce807b541428c",
+    "url": "/code-test-redux/static/js/main.489defa0.chunk.js"
+  },
+  {
+    "revision": "02ab1fb96642e7665e77",
+    "url": "/code-test-redux/static/js/runtime~main.24aa5ed8.js"
+  }
+]);
